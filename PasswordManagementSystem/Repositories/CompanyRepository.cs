a0ff41ca123524974d19a4c733462f10d469ad73
Ahmet Furkan Ekinci <93397508+Furkan2001@users.noreply.github.com>
2024-07-19 10:32:31 +0300

    Add files via upload

---
+++ PasswordManagementSystem/Repositories/CompanyRepository.cs
@@ -0,0 +1,47 @@
+﻿using Dapper;
+using PasswordManagementSystem.Interfaces;
+using PasswordManagementSystem.Models;
+using System.Collections.Generic;
+using System.Data;
+using System.Threading.Tasks;
+
+namespace PasswordManagementSystem.Repositories
+{
+    public class CompanyRepository : ICompanyRepository
+    {
+        private readonly IDbConnection _db;
+
+        public CompanyRepository(IDbConnection db)
+        {
+            _db = db;
+        }
+
+        public async Task<IEnumerable<Company>> GetAll()
+        {
+            return await _db.QueryAsync<Company>("SELECT * FROM Companies");
+        }
+
+        public async Task<Company> GetById(int id)
+        {
+            return await _db.QueryFirstOrDefaultAsync<Company>("SELECT * FROM Companies WHERE CompanyId = @Id", new { Id = id });
+        }
+
+        public async Task Add(Company entity)
+        {
+            var sql = "INSERT INTO Companies (CompanyName, Domain) VALUES (@CompanyName, @Domain)";
+            await _db.ExecuteAsync(sql, entity);
+        }
+
+        public async Task Update(Company entity)
+        {
+            var sql = "UPDATE Companies SET CompanyName = @CompanyName, Domain = @Domain WHERE CompanyId = @CompanyId";
+            await _db.ExecuteAsync(sql, entity);
+        }
+
+        public async Task Delete(int id)
+        {
+            var sql = "DELETE FROM Companies WHERE CompanyId = @Id";
+            await _db.ExecuteAsync(sql, new { Id = id });
+        }
+    }
+}
---
+++ PasswordManagementSystem/Repositories/LabelRepository.cs
@@ -0,0 +1,98 @@
+﻿using Dapper;
+using PasswordManagementSystem.Interfaces;
+using PasswordManagementSystem.Models;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Threading.Tasks;
+
+namespace PasswordManagementSystem.Repositories
+{
+    public class LabelRepository : ILabelRepository
+    {
+        private readonly IDbConnection _dbConnection;
+
+        public LabelRepository(IDbConnection dbConnection)
+        {
+            _dbConnection = dbConnection;
+        }
+
+        public async Task<IEnumerable<Label>> GetAll()
+        {
+            var query = "SELECT * FROM Label";
+            return await _dbConnection.QueryAsync<Label>(query);
+        }
+
+        public async Task<Label> GetById(int id)
+        {
+            var query = "SELECT * FROM Label WHERE LabelId = @LabelId";
+            return await _dbConnection.QuerySingleOrDefaultAsync<Label>(query, new { LabelId = id });
+        }
+
+        public async Task Add(Label entity)
+        {
+            var query = "INSERT INTO Label (UserId, LabelText) VALUES (@UserId, @LabelText)";
+            await _dbConnection.ExecuteAsync(query, entity);
+        }
+
+        public async Task Update(Label entity)
+        {
+            var query = "UPDATE Label SET LabelText = @LabelText WHERE LabelId = @LabelId";
+            await _dbConnection.ExecuteAsync(query, entity);
+        }
+
+        public async Task Delete(int id)
+        {
+            var query = "DELETE FROM Label WHERE LabelId = @LabelId";
+            await _dbConnection.ExecuteAsync(query, new { LabelId = id });
+        }
+
+        public async Task<IEnumerable<Label>> GetUserLabelsAsync(int userId)
+        {
+            var query = "SELECT * FROM Label WHERE UserId = @UserId";
+            return await _dbConnection.QueryAsync<Label>(query, new { UserId = userId });
+        }
+
+        public async Task AddAsync(Label label)
+        {
+            var query = "INSERT INTO Label (UserId, PasswordId, LabelText) VALUES (@UserId, @PasswordId, @LabelText)";
+            await _dbConnection.ExecuteAsync(query, label);
+        }
+
+        public async Task UpdateAsync(Label label)
+        {
+            var query = "UPDATE Label SET LabelText = @LabelText WHERE UserId = @UserId AND PasswordId = @PasswordId";
+            await _dbConnection.ExecuteAsync(query, label);
+        }
+
+        public async Task<Label> GetByPasswordIdAndUserId(int passwordId, int userId)
+        {
+            var query = "SELECT * FROM Label WHERE PasswordId = @PasswordId AND UserId = @UserId";
+            return await _dbConnection.QuerySingleOrDefaultAsync<Label>(query, new { PasswordId = passwordId, UserId = userId });
+        }
+
+        public async Task DeleteLabelsByPasswordIdAsync(int passwordId)
+        {
+            var sql = "DELETE FROM Label WHERE PasswordId = @PasswordId";
+            await _dbConnection.ExecuteAsync(sql, new { PasswordId = passwordId });
+        }
+
+        public async Task<bool> DeleteLabelByPasswordIdAndUserIdAsync(int passwordId, int userId)
+        {
+            // Etiketin var olup olmadığını kontrol et
+            var checkSql = "SELECT COUNT(1) FROM Label WHERE PasswordId = @PasswordId AND UserId = @UserId";
+            var exists = await _dbConnection.ExecuteScalarAsync<bool>(checkSql, new { PasswordId = passwordId, UserId = userId });
+
+            if (exists)
+            {
+                // Eğer etiket varsa, sil
+                var deleteSql = "DELETE FROM Label WHERE PasswordId = @PasswordId AND UserId = @UserId";
+                var result = await _dbConnection.ExecuteAsync(deleteSql, new { PasswordId = passwordId, UserId = userId });
+                return result > 0;
+            }
+
+            // Eğer etiket yoksa, silme işlemi yapılmaz
+            return true;
+        }
+    }
+}
